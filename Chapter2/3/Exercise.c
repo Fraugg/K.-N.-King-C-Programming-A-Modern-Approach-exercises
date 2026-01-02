3655cf1e50761fce78c8319c2edb0d238c644308
Fraugg <sondog6666@gmail.com>
2026-01-02 12:25:13 +0100

    Create Exercise.c

---
+++ Chapter2/3/Exercise.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main(void)
+{
+    int r;
+    float p = 3.14159f, v;
+
+    printf("Enter radius of the sphere: ");
+    scanf("%d", &r);
+
+    v = (4.0f / 3.0f) * p * (r * r * r); // 4/3 give us result 1, not 1,333
+    
+    printf("Volume of the sphere is: %.3fm^3\n", v);
+
+    return 0;
+}
+
+
